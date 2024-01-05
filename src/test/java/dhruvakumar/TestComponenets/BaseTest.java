f641632f65018df06edbef9408ce68ac2508f581
dhruvakumarkr <dhruvakumarkr45@gmail.com>
2024-01-05 22:32:40 +0530

    second person first commit

--- src/test/java/dhruvakumar/TestComponenets/BaseTest.java
+++ src/test/java/dhruvakumar/TestComponenets/BaseTest.java
@@ -131,7 +131,7 @@ public class BaseTest {
 
 	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
-		System.out.println("Test is Done thank you");
+		System.out.println("Test");
 		driver.quit();
 	}
 }
